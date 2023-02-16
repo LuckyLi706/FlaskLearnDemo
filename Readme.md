f1e79addb57b88cd46a4d9e05ee37b275e9a84a6
lijie <lijie2@znlh.com>
2023-02-16 20:43:56 +0800

    socketio实现聊天室

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Flask学习demo
++ [普通请求](app.py)
++ [定时任务](app_scheduler_task.py)
++ [websocket](websocket.py)
++ [socketio实现聊天室](socket_io.py)
--- socket_io.py
+++ socket_io.py
@@ -2,33 +2,42 @@ from flask import Flask, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, send
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "secret!"
 socketio = SocketIO(app)
 
 
-@socketio.on("room_1")
-def handle_event(message):
-    request.sid
-    print("服务器已经接收到消息：" + message)
-    emit("room_1", "服务器已经接收到消息：" + message)
+# 自定义事件
+@socketio.on("information")
+def handle_event(*message):
+    user_id = message[0]
+    room = message[1]
+    message_info = message[2]
+    # 向房间号所有的用户发消息，不包括自己
+    print(f'{user_id} to room:{room} send {message_info}')
+    emit("information", message_info, to=room, include_self=False)
 
 
 # 房间的概念，加入房间
 @socketio.on('join')
-def join(data):
-    username = data['username']
-    room = data['room']
+def join(*data):
+    user_id = data[0]
+    room = data[1]
     join_room(room)
-    send(f'{username} has entered the room', to=room)
+    # send为内置普通事件，客户端必须使用event='message'去接收消息
+    # 向房间号所有的用户发消息，不包括自己
+    print(f'{user_id} user has entered the room')
+    send(f'{user_id} user has entered the room', to=room, include_self=False)
 
 
 # 房间的概念，离开房间
 @socketio.on('leave')
-def leave(data):
-    username = data['username']
-    room = data['room']
+def leave(*data):
+    user_id = data[0]
+    room = data[1]
     leave_room(room)
-    send(f'{username} has left the room', to=room)
+    # send为内置普通事件，客户端必须使用event='message'去接收消息
+    # 向房间号所有的用户发消息，不包括自己
+    print(f'{user_id} user has left the room')
+    send(f'{user_id} user has left the room', to=room)
 
 
 if __name__ == "__main__":
